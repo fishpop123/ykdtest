59efc160daeb97bbd0f9f9fbd359e0d936f534e5
wx <2040675557@qq.com>
2026-07-15 14:11:31 +0800

    Initial commit: Spring Boot项目初始化

---
+++ src/main/java/com/example/ykd/YkdApplication.java
@@ -0,0 +1,14 @@
+package com.example.ykd;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YkdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YkdApplication.class, args);
+	}
+
+}
